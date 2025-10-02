f0f47c1a93f533825c485bcf552b4fd9c2beb012
Vaishnavi <vaishnavitiwari104@gmail.com>
2025-10-02 17:23:29 +0530

    Merge Conflict

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
-});const express = require('express');
+});
 
 
 
